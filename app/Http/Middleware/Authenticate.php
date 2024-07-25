38a28dcfdd4860171cc15dda01c76fe20410fe61
Orifjon-github <avazbekoripov94@gmail.com>
2024-07-25 17:25:41 +0500

    unauthorized error

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Middleware;
 
+use App\Helpers\Response;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
+    use Response;
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        return $request->expectsJson() ? null : $this->error('Unauthorized', 401);
     }
 }
