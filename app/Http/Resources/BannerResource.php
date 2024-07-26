bb2e7e9d28c60f3ab43e7d6b553c3e22fea2f314
Orifjon-github <avazbekoripov94@gmail.com>
2024-07-26 16:35:08 +0500

    jpg to png

--- app/Http/Resources/BannerResource.php
+++ app/Http/Resources/BannerResource.php
@@ -5,23 +5,21 @@ namespace App\Http\Resources;
 use App\Helpers\Helpers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\App;
 
 /**
  * @property mixed $id
+ * @property mixed $image
  */
 class BannerResource extends JsonResource
 {
     use Helpers;
     public function toArray(Request $request): array
     {
-        $language = App::getLocale();
-//        $title = match ($language) {'ru' => $this->title, 'uz' => $this->title_uz ?? $this->title, 'en' => $this->title_en ?? $this->title};
-        $image = match ($language) {'ru' => $this->image, 'uz' => $this->image_uz ?? $this->image, 'en' => $this->image_en ?? $this->image};
         return [
             'id' => $this->id,
-            'title' => $this->getValue($this, 'title'),
-            'image' => env('IMAGES_BASE_URL') . $image,
+            'title' => $this->getValue($this),
+            'description' => $this->getValue($this, 'description'),
+            'image' => env('IMAGES_BASE_URL') . $this->image,
         ];
     }
 }
--- database/seeders/AdvantageSeeder.php
+++ database/seeders/AdvantageSeeder.php
@@ -16,22 +16,22 @@ class AdvantageSeeder extends Seeder
         Advantage::create([
             'title' => "Professional employee",
             'description' => 'We strictly adhere to international food safety standards, ensuring you have products you can trust.',
-            'icon' => 'uploads/advantage1.jpg'
+            'icon' => 'uploads/advantage1.png'
         ]);
         Advantage::create([
             'title' => "Commitment",
             'description' => 'We strictly adhere to international food safety standards, ensuring you have products you can trust.',
-            'icon' => 'uploads/advantage2.jpg'
+            'icon' => 'uploads/advantage2.png'
         ]);
         Advantage::create([
             'title' => 'Verified',
             'description' => 'We strictly adhere to international food safety standards, ensuring you have products you can trust.',
-            'icon' => 'uploads/advantage3.jpg'
+            'icon' => 'uploads/advantage3.png'
         ]);
         Advantage::create([
             'title' => '24/7 support',
             'description' => 'We strictly adhere to international food safety standards, ensuring you have products you can trust.',
-            'icon' => 'uploads/advantage4.jpg'
+            'icon' => 'uploads/advantage4.png'
         ]);
     }
 }
