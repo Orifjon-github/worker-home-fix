8893caa50758dea7a92ff421d288ba01db5a6a3e
Orifjon-github <avazbekoripov94@gmail.com>
2024-08-22 17:48:08 +0500

    send question

--- database/migrations/2024_08_08_182216_create_user_questions_table.php
+++ database/migrations/2024_08_08_182216_create_user_questions_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('user_questions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
             $table->text('question');
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
