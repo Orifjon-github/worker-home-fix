510952439e800acb437c3db12efaddf1ede17685
alisherodilov005 <93549542+alisherodilov2@users.noreply.github.com>
2024-10-27 02:22:33 +0500

    chanegs

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,36 +14,38 @@ class TaskController extends Controller
 
     public function me()
     {
-        return $this->success(my()->getTasks()->where('status', 'finished'));
+        return $this->success(my()->getTasks());
     }
 
     public function index(Request $request)
     {
-        if ($request->status == 'now') {
-            return $this->success(my()->getTasks()->where('created_at', '=', Carbon::today()));
-        }
         return $this->success(my()->getTasks()->where('status', $request->status));
     }
-    public function  show($id)
+
+    public function show($id)
     {
         return $this->success(new TaskDetailResource(Task::find($id)));
     }
-    public function  getEquipment(Request $request)
+
+    public function getEquipment(Request $request)
     {
         $task = Task::find($request->task_id);
         $task->update(['is_equipment' => $task->is_equipment == 1 ? 0 : 1]);
+
         return $this->success($task);
     }
-    public  function  updateDate(Request $request)
+
+    public function updateDate(Request $request)
     {
         $task = Task::find($request->task_id);
         $dateTime = Carbon::now()->format('d-m.Y H:i'); // Outputs: '15-10.2024 19:00'
-        if($request->start_time){
-            $task->update(['start_time' => $dateTime ]);
+        if ($request->start_time) {
+            $task->update(['start_time' => $dateTime]);
         }
-        if($request->end_time){
-            $task->update(['end_time' =>  $dateTime]);
+        if ($request->end_time) {
+            $task->update(['end_time' => $dateTime]);
         }
+
         return $this->success($task);
     }
 }
